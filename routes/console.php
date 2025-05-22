0caf26f5760bca139d48a11052daa9069be5295f
jimmy <r567twjob@gmail.com>
2025-05-22 14:17:16 +0800

    Add scheduled task logging and enable inspire command every minute

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+
+// Schedule::command('inspire')->everyMinute()->purpose('Run the scheduled tasks');
+Schedule::call(function () {
+    Log::info("This is a scheduled task running every minute.");
+})->everyMinute();
+
+Schedule::command('inspire')
+    ->everyMinute();
